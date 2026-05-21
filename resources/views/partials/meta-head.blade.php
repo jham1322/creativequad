a5c7c8301806bf79669fb66728fa00c9fe807ea1
jham1322 <jham1322@users.noreply.github.com>
2026-05-22 04:34:13 +0800

    Update site favicon

--- resources/views/partials/meta-head.blade.php
+++ resources/views/partials/meta-head.blade.php
@@ -4,13 +4,15 @@
     $seoRobots = $robots ?? 'index,follow';
     $seoCanonical = $canonical ?? url()->current();
     $seoImage = $image ?? asset('images/hero/fgg.webp');
+    $faviconVersion = @filemtime(public_path('favicon.png')) ?: time();
 @endphp
 <title>{{ $seoTitle }}</title>
 <meta name="description" content="{{ $seoDescription }}">
 <meta name="robots" content="{{ $seoRobots }}">
 <link rel="canonical" href="{{ $seoCanonical }}">
-<link rel="icon" type="image/png" href="{{ asset('favicon.png') }}">
-<link rel="apple-touch-icon" href="{{ asset('apple-touch-icon.png') }}">
+<link rel="icon" type="image/png" href="{{ asset('favicon.png') }}?v={{ $faviconVersion }}">
+<link rel="shortcut icon" href="{{ asset('favicon.ico') }}?v={{ $faviconVersion }}">
+<link rel="apple-touch-icon" href="{{ asset('apple-touch-icon.png') }}?v={{ $faviconVersion }}">
 <meta property="og:type" content="website">
 <meta property="og:title" content="{{ $seoTitle }}">
 <meta property="og:description" content="{{ $seoDescription }}">
